52a832d8c3a0c9c4c99da2879310bd98da305c83
Al-Serra <mathematic104@gmail.com>
2023-08-10 14:40:35 -0700

    task4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - a special & first function that gets excueted
+* int - specifies the return type as an intger
+* viod - indicates that main takes no parameters
+* puts - a function used to output a string
+* Return: zero, the program executed successfully with no errors
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
