668f7909fe8068f5d22afc96f2efbeeefb0fcf30
Al-Serra <mathematic104@gmail.com>
2023-08-10 14:59:59 -0700

    task5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the first function that gets executed, the entry point for the pogram
+ * printf - a function that allows you to control the formatting of the output
+ * Return: zero, the program executed successfully without any errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
