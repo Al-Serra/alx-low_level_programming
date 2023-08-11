cc41c3897b3a66000ae80cd76c81fb2d1fca2a00
Al-Serra <mathematic104@gmail.com>
2023-08-11 16:59:12 -0700

    task2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The Entry Function
+ *
+ * Return: Zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
