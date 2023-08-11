5fd48e95354119561aedea13070d0531a57654f7
Al-Serra <mathematic104@gmail.com>
2023-08-10 18:21:13 -0700

    task8- 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 
 /**
  * main - the entry function
+ *
  * Return: one, returning an error status
  */
 
 int main(void)
 {
-       	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-       	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
