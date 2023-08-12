2a032980baf91781a57e8f7531a48a713c7ea25d
Al-Serra <mathematic104@gmail.com>
2023-08-12 08:51:56 -0700

    task7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The Entry Function
+ * Return: Zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
