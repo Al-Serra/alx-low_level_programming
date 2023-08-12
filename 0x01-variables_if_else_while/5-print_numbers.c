c4120fda804567d1f92da466fb47fb0e97952280
Al-Serra <mathematic104@gmail.com>
2023-08-12 08:29:33 -0700

    task./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The Entry Function
+ * Return: Zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
