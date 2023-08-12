c50768b003766a03b3deceb7834482db5b4a46d8
Al-Serra <mathematic104@gmail.com>
2023-08-12 15:49:35 -0700

    task./101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - different combinations of three digits,
+ * separated by ,, followed by a space
+ */
+int main(void)
+{
+	int i, j, m;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (m = j + 1; m < 10; m++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((m % 10) + '0');
+
+				if (i == 7 && j == 8 && m == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
