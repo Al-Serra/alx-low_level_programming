314499ef31ac7d2f8c29aa6b62aeee93303cc2bc
Al-Serra <mathematic104@gmail.com>
2023-08-15 17:24:25 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,19 +5,34 @@
  */
 void times_table(void)
 {
-	int s, k;
+	int s, k, p, i, m;
 
 	for (s = 0; s <= 9; s++)
 	{
 		for (k = 0; k <= 9; k++)
 		{
-			if (k != 0)
+			p = s * k;
+			if (p > 9)
 			{
+				i = p % 10;
+				m = (p - i) / 10;
+
 				_putchar(',');
 				_putchar(' ');
+				_putchar(m + '0');
+				_putchar(i + '0');
 			}
-			_putchar('0' + (s * k) / 10);
-			_putchar('0' + (s * k) % 10);
+			else
+			{
+				if (k != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+				_putchar(p + '0');
+			}
 		}
 		_putchar('\n');
 	}
