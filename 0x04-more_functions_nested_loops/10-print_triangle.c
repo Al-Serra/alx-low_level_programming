0cb9723364049cb4938581f601aedf158ca76a45
Al-Serra <mathematic104@gmail.com>
2023-08-17 18:55:48 -0700

    task10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: The size of the tringle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int r, s, h;
+
+		for (r = 1; r <= size; r++)
+		{
+			for (s = size - r; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (h = 1; h <= r; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
