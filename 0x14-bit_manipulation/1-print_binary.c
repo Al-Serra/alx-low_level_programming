0a45dfe905ea7d0cbd65d204fe95e2e9b4e33f53
Al-serra <mathematic104@gmail.com>
2023-09-28 07:11:06 -0700

    task_1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,20 +5,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	int s, count = 0;
-	unsigned long int current;
-
-	for (s = 63; s >= 0; s++)
+	if (n == 0)
 	{
-		current = n >> s;
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
 		_putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		_putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
