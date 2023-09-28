95b20ff46b566ee4d039522317d19d6f3941fde7
Al-serra <mathematic104@gmail.com>
2023-09-28 06:50:27 -0700

    task0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
+	unsigned int decimal;
+	unsigned int s;
 
-	if (b == NULL)
-		return (0);
-	while (*b != '\0')
+	for (decimal = 0, s = 0; b[s] != '\0'; s++)
 	{
-		value = value << 1;
-		if (*b != '1' && *b != '0')
+		if (b[s] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[s] == '0')
+			decimal <<= 1;
+		else if (b[s] != '0' && b[s] != '1')
 			return (0);
-		else if (*b == '1')
-			value = value | 1;
-		b++;
 	}
-	return (value);
+
+	return (decimal);
 }
